5b7556c0d2a62be68595dacedf174756a2f2847d
matheus695p <mateu_695p@hotmail.com>
2021-04-18 12:33:09 -0400

    add: redes fully connected

---
+++ src/nn.py
@@ -0,0 +1,102 @@
+import torch.nn as nn
+# import torch.nn.functional as F
+
+
+class MulticlassClassification(nn.Module):
+    """
+    Construimos una red neuronal fully connected
+
+    ...
+    Attributes
+    ----------
+    object : herencia
+        módulo de redes neuronales de pytorch
+
+    Methods
+    -------
+    forward(x):
+        propagar hacia adelante la red.
+    """
+
+    def __init__(self, num_feature, num_class):
+        super(MulticlassClassification, self).__init__()
+
+        self.layer_1 = nn.Linear(num_feature, 512)
+        self.layer_2 = nn.Linear(512, 128)
+        self.layer_3 = nn.Linear(128, 64)
+        self.layer_out = nn.Linear(64, num_class)
+
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.2)
+        self.batchnorm1 = nn.BatchNorm1d(512)
+        self.batchnorm2 = nn.BatchNorm1d(128)
+        self.batchnorm3 = nn.BatchNorm1d(64)
+
+    def forward(self, x):
+        x = self.layer_1(x)
+        x = self.batchnorm1(x)
+        x = self.relu(x)
+
+        x = self.layer_2(x)
+        x = self.batchnorm2(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+
+        x = self.layer_3(x)
+        x = self.batchnorm3(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+
+        x = self.layer_out(x)
+
+        return x
+
+
+class GalaxiesNet(nn.Module):
+    """
+    Construimos una red neuronal fully connected
+
+    ...
+    Attributes
+    ----------
+    object : herencia
+        módulo de redes neuronales de pytorch
+
+    Methods
+    -------
+    forward(x):
+        propagar hacia adelante la red.
+    """
+
+    def __init__(self, num_feature, num_class):
+        super(GalaxiesNet, self).__init__()
+
+        self.layer_1 = nn.Linear(num_feature, 512)
+        self.layer_2 = nn.Linear(512, 128)
+        self.layer_3 = nn.Linear(128, 64)
+        self.layer_out = nn.Linear(64, num_class)
+
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.2)
+        self.batchnorm1 = nn.BatchNorm1d(512)
+        self.batchnorm2 = nn.BatchNorm1d(128)
+        self.batchnorm3 = nn.BatchNorm1d(64)
+
+    def forward(self, x):
+        x = self.layer_1(x)
+        x = self.batchnorm1(x)
+        x = self.relu(x)
+
+        x = self.layer_2(x)
+        x = self.batchnorm2(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+
+        x = self.layer_3(x)
+        x = self.batchnorm3(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+
+        x = self.layer_out(x)
+
+        return x
